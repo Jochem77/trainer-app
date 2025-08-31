73e9ed2534aaa594a82abe5cceb86f9587caacde
Jochem77 <jochem.steenbakkers@gmail.com>
2025-08-31 22:53:05 +0200

    fix(ts): remove unused NotFound to satisfy TS6133

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-function NotFound() {
-  // Als iemand op een subroute landt, toon gewoon de app of link naar home
-  return (
-    <div style={{padding:24,fontFamily:'system-ui'}}>
-      <h3>Pagina niet gevonden</h3>
-      <p>Ga terug naar de <a href="/trainer-app/">home</a>.</p>
-    </div>
-  )
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/trainer-app/">
       <Routes>
         <Route path="/" element={<App />} />
-        {/* Fallback: elk onbekend pad -> App (of <NotFound /> als je wilt) */}
+        {/* catch-all naar App */}
         <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
